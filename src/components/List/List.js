5ea98cb088773adb52220fa047b80c1750993c18
kalel <kalelbotv4@outlook.com>
2022-11-18 09:22:35 +0530

    Added cards

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Paper, CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Title from './Title';
+import Card from '../Card';
+import InputContainer from '../Input/InputContainer';
 
 const useStyle = makeStyles((theme)=>({
     root:{
@@ -12,6 +14,8 @@ const useStyle = makeStyles((theme)=>({
 }
 
 ));
+
+
 export default function List() {
     const classes = useStyle();
     return (
@@ -19,6 +23,10 @@ export default function List() {
             <Paper className={classes.root}>
                 <CssBaseline />
                 <Title />
+                <Card />
+                <Card />
+                <Card />
+                <InputContainer />
             </Paper>
         </div>
   );
